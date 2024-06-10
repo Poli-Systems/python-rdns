37bcd21abcea5adf262fcef36dc60de95a8bf80a
Poli <pr@polisystems.ch>
2024-06-10 09:02:59 +0200

    First commit

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
-# python-rdns
- 
+# Welcome to IP Address DNS
+
+This domain redirects all subdomains to a specific IP address if an IP is appended to it.
+
+## Examples
+
+- **1.2.3.4.polisystems.cloud** is an A record for **1.2.3.4**
+- **1.2.3.4.reverse.polisystems.cloud** is an A reversed record for **4.3.2.1**
+- **anysubdomain.1.2.3.4.polisystems.cloud** is an A reversed record for **1.2.3.4**
+- **anysubdomain.1.2.3.4.reverse.polisystems.cloud** is an A reversed record for **4.3.2.1**
+
+You can find the GitHub repository detailing how it works [here](https://github.com/Poli-Systems/python-rdns/tree/master).
+
+While not flawless, this code functions effectively and without complications for our reverse PTR records. Feel free to utilize it for services beyond our corporation.
+
+We took this idea that was originally created by Sam Stephenson at Basecamp for XIP-DNS and now it was remade in Python by Poli Systems GmbH.
+
+To learn more about our hosting company, visit our [website](https://polisystems.ch).
+
+![Poli Systems Logo](https://cdn.polisystems.ch/assets/images/logo/Blue-Text-Logo.png)
---
+++ ReverseDnsServer.py
@@ -0,0 +1,108 @@
+# Created by Philippe Raimundo from Poli Systems GmbH
+# 2024 Poli Systems GmbH
+
+from dnslib import DNSRecord, DNSHeader, RR, QTYPE, A, AAAA, SOA, NS, CAA
+from dnslib.server import DNSServer, BaseResolver
+import re
+import datetime
+import time  # Import the time module
+
+# Variables for the domain and IP addresses
+DOMAIN = 'polisystems.cloud'
+IPV4_ADDRESS = '*.*.*.*'  # Replace with your desired IPv4 address
+IPV6_ADDRESS = '*:*:*:*::1'  # Replace with your desired IPv6 address
+
+# SOA record details
+SOA_MNAME = '*.*.*.*'  # Primary master name server
+SOA_RNAME = '*.*.*'  # Responsible party email (replace "@" with ".")
+SOA_REFRESH = 3600  # Refresh interval in seconds
+SOA_RETRY = 1800  # Retry interval in seconds
+SOA_EXPIRE = 1209600  # Expire interval in seconds
+SOA_MINIMUM = 86400  # Minimum TTL in seconds
+
+# NS record details
+NS_SERVERS = ['*.*.*.*', '*.*.*.*']  # List of name servers
+
+# Default TTL for all records (7 days in seconds)
+TTL = 604800
+
+# CAA record details
+CAA_FLAGS = 0
+CAA_TAG = 'issue'
+CAA_VALUE = 'letsencrypt.org'
+
+# Global counter for DNS queries
+dns_query_counter = 0
+
+# Function to generate a new serial number based on the current date and time
+def generate_serial():
+    now = datetime.datetime.now()
+    return int(now.strftime('%Y%m%d%H%M%S'))  # YYYYMMDDHHMMSS format
+
+# Function to update the stats.txt file
+def update_stats():
+    with open('/root/DNS/stats.txt', 'w') as f:
+        f.write(f"DNS Queries: {dns_query_counter}\n")
+
+class CustomResolver(BaseResolver):
+    def resolve(self, request, handler):
+        global dns_query_counter
+        dns_query_counter += 1
+        update_stats()
+
+        reply = request.reply()
+        qname = request.q.qname
+        qtype = QTYPE[request.q.qtype]
+        domain = str(qname).strip('.').lower()  # Normalize the domain to lower case
+
+        print(f"Request for domain: {domain} of type: {qtype}")
+
+        # Update the SOA serial number to the current date and time
+        SOA_SERIAL = generate_serial()
+
+        # Adjusted regex patterns to handle any subdomain and suffix after the target domain
+        direct_match = re.match(r'^(.*\.)?(\d+)\.(\d+)\.(\d+)\.(\d+)\.' + re.escape(DOMAIN) + '$', domain)
+        reverse_match = re.match(r'^(.*\.)?(\d+)\.(\d+)\.(\d+)\.(\d+)\.reverse\.' + re.escape(DOMAIN) + '$', domain)
+
+        if direct_match:
+            ip_address = ".".join(direct_match.groups()[-4:])
+            print(f"Direct match found: {ip_address}")
+            reply.add_answer(RR(qname, QTYPE.A, rdata=A(ip_address), ttl=TTL))
+        elif reverse_match:
+            ip_address = ".".join(reverse_match.groups()[-4:][::-1])
+            print(f"Reverse match found: {ip_address}")
+            reply.add_answer(RR(qname, QTYPE.A, rdata=A(ip_address), ttl=TTL))
+        elif domain == DOMAIN:
+            print(f"Root domain query for {DOMAIN}")
+            if qtype == 'A':
+                reply.add_answer(RR(qname, QTYPE.A, rdata=A(IPV4_ADDRESS), ttl=TTL))
+            elif qtype == 'AAAA':
+                reply.add_answer(RR(qname, QTYPE.AAAA, rdata=AAAA(IPV6_ADDRESS), ttl=TTL))
+            elif qtype == 'SOA':
+                soa_record = SOA(mname=SOA_MNAME,
+                                 rname=SOA_RNAME,
+                                 times=(SOA_SERIAL, SOA_REFRESH, SOA_RETRY, SOA_EXPIRE, SOA_MINIMUM))
+                reply.add_answer(RR(qname, QTYPE.SOA, rdata=soa_record, ttl=TTL))
+            elif qtype == 'NS':
+                for ns in NS_SERVERS:
+                    reply.add_answer(RR(qname, QTYPE.NS, rdata=NS(ns), ttl=TTL))
+            elif qtype == 'CAA':
+                caa_record = CAA(flags=CAA_FLAGS, tag=CAA_TAG, value=CAA_VALUE)
+                reply.add_answer(RR(qname, QTYPE.CAA, rdata=caa_record, ttl=TTL))
+            else:
+                print(f"Unhandled query type: {qtype}")
+        else:
+            print("No match found")
+
+        return reply
+
+if __name__ == '__main__':
+    resolver = CustomResolver()
+    server = DNSServer(resolver, port=53, address='0.0.0.0')
+    server.start_thread()
+
+    try:
+        while True:
+            time.sleep(0.1)  # Add a sleep interval to reduce CPU usage
+    except KeyboardInterrupt:
+        pass
